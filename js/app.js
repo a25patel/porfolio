cf94d4216826d49fcef7d242b52fde20b0376b50
a25patel <aanaletap2510@gmail.com>
2015-09-27 17:46:16 -0600

    layout complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,48 @@
-console.log('hello');
+// 1. toggle function using a simple jquery
+function design(){
+  $('.designInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.designInfo').hide();
+  $('.design').click(design)
+});
+
+function link(){
+  $('.linksInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.linksInfo').hide();
+  $('.links').click(link)
+});
+
+function resume(){
+  $('.resumeInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.resumeInfo').hide();
+  $('.resume').click(resume)
+});
+
+function develop(){
+  $('.developmentInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.developmentInfo').hide();
+  $('.development').click(develop)
+});
+
+function about(){
+  $('.aboutInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.aboutInfo').hide();
+  $('.about').click(about)
+});
+
+function contact(){
+  $('.contactInfo').toggle('slow');
+}
+$(document).ready(function click(){
+  $('.contactInfo').hide();
+  $('.contact').click(contact)
+});
